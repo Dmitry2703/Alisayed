58c9123be17c51392970a5060ea7a73d1043ff02
Dmitry Meshcheryakov <dmitri.meshcheryakov@gmail.com>
2015-05-21 12:21:57 +0600

    Попытка именования классов по методологии БЭМ.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
 // Menu Open and Close
 
-var toggle = document.querySelector(".menu-toggle");
-var inner = document.querySelector(".menu-inner");
+var toggle = document.querySelector(".menu__toggle");
+var inner = document.querySelector(".menu__inner");
 var body = document.querySelector("body");
 
 toggle.addEventListener("click", function(event) {
 event.preventDefault();
 
-    if (inner.classList.contains("menu-inner-show")) {
+    if (inner.classList.contains("menu__inner_state_show")) {
             body.classList.remove("menu-opened");
-            inner.classList.remove("menu-inner-show");
+            inner.classList.remove("menu__inner_state_show");
           }
                       else { 
                         body.classList.add("menu-opened");
-                        inner.classList.add("menu-inner-show");
+                        inner.classList.add("menu__inner_state_show");
 }
 });
 
 window.addEventListener("keydown", function(event) {
         if (event.keyCode == 27) {
-          if (inner.classList.contains("menu-inner-show")) {
-            inner.classList.remove("menu-inner-show");
+          if (inner.classList.contains("menu__inner_state_show")) {
+            inner.classList.remove("menu__inner_state_show");
           }
         }
       });
