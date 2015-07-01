d1cef5b09c99e774048ac5dc14191fb52d07c768
Dmitry Meshcheryakov <dmitri.meshcheryakov@gmail.com>
2015-07-01 19:07:23 +0600

    Последняя версия

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,15 @@
 // Menu Open and Close
 
-var toggle = document.querySelector(".menu__toggle");
-var inner = document.querySelector(".menu__inner");
-var body = document.querySelector("body");
+var toggle = document.querySelector(".js-menu__toggle");
+var body = document.body;
 
 toggle.addEventListener("click", function(event) {
-event.preventDefault();
-
-    if (inner.classList.contains("menu__inner_state_show")) {
-            body.classList.remove("menu-opened");
-            inner.classList.remove("menu__inner_state_show");
-          }
-                      else { 
-                        body.classList.add("menu-opened");
-                        inner.classList.add("menu__inner_state_show");
-}
+  event.preventDefault();
+  body.classList.toggle("menu-opened");
 });
 
 window.addEventListener("keydown", function(event) {
-        if (event.keyCode == 27) {
-          if (inner.classList.contains("menu__inner_state_show")) {
-            inner.classList.remove("menu__inner_state_show");
-          }
-        }
-      });
+  if (event.keyCode == 27) {
+    body.classList.remove("menu-opened");
+  }
+});
